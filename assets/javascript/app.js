6b7057105e36f8fac0f3ea40baf4ae1e2b4b3c8f
Michael Piatnichko <mpiatnichko@gmail.com>
2017-06-23 12:03:35 -0600

    html layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyADWG5OhU1WOSjS2gONyNJ442ntIgxxLNI",
+    authDomain: "train-schedule-7bb7b.firebaseapp.com",
+    databaseURL: "https://train-schedule-7bb7b.firebaseio.com",
+    projectId: "train-schedule-7bb7b",
+    storageBucket: "train-schedule-7bb7b.appspot.com",
+    messagingSenderId: "623013463036"
+  };
+  firebase.initializeApp(config);
+
+  
